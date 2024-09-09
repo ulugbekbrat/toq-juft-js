2d132365affc0110d8c6dbe621a2f4daaa1a07f9
ulugbekbrat <168886327+ulugbekbrat@users.noreply.github.com>
2024-09-09 14:25:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let a = +prompt('lyuboy son kiriting');
+
+
+let div1 = document.querySelector('.div1');
+
+
+
+
+
+
+let div2 = document.querySelector('.div2');
+
+if (a % 2 === 0) {
+
+
+    div1.querySelector('h1').textContent = a;
+} else {
+
+
+
+    div2.querySelector('h1').textContent = a;
+
+
+}
